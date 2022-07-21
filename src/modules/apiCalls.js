5e6331ea09b4c0070bbef231073a827910e44beb
AndrewLin95 <andrew.lin88@hotmail.com>
2022-07-20 21:41:01 -0400

    separated the API calls for getLatLon and added a hourly weather API call

--- src/modules/apiCalls.js
+++ src/modules/apiCalls.js
@@ -1,25 +1,52 @@
-import {updatePage} from "./display"
+import { updateTopPage, updateBottomPage } from "./display"
 
+// google maps API + debouncing, limiting the # of calls. prevents spam
+
+let weatherGeocode = '';
 let currentWeatherData = '';
+let hourlyWeatherData = '';
 
-async function getCurrentWeather(location) {
+async function getLatLon(location){
     try {
         const response = await fetch(`http://api.openweathermap.org/geo/1.0/direct?q=${location}&appid=59d8f83c0d0672671941c70c99060910`, {mode:'cors'});
-        const weatherGeocode = await response.json();
-
-        const weatherResponse = await fetch (`https://api.openweathermap.org/data/2.5/weather?lat=${weatherGeocode[0].lat}&lon=${weatherGeocode[0].lon}&appid=59d8f83c0d0672671941c70c99060910`)
-        const weatherData = await weatherResponse.json();
-
-        currentWeatherData = weatherData;
-        console.log(currentWeatherData);     // TO REMOVE 
-        updatePage();
+        weatherGeocode = await response.json();
+        getCurrentWeather();
+        getWeeklyWeather();
     } catch (err) {
         console.log(err);
     }
 };
 
-function callCurrentData() {
-    return currentWeatherData;
+async function getCurrentWeather() {
+    try {
+        const weatherResponse = await fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${weatherGeocode[0].lat}&lon=${weatherGeocode[0].lon}&appid=59d8f83c0d0672671941c70c99060910`, {mode:'cors'});
+        currentWeatherData = await weatherResponse.json();
+
+        console.log(currentWeatherData);     // TO REMOVE 
+        updateTopPage();
+    } catch (err) {
+        console.log(err);
+    }
+};
+
+async function getWeeklyWeather() {
+    try {
+        const weeklyResponse = await fetch(`https://api.openweathermap.org/data/2.5/forecast?lat=${weatherGeocode[0].lat}&lon=${weatherGeocode[0].lon}&appid=59d8f83c0d0672671941c70c99060910`, {mode:'cors'});
+        hourlyWeatherData = await weeklyResponse.json();
+        
+        console.log(hourlyWeatherData);
+        updateBottomPage();
+    } catch (err) {
+        console.log(err);
+    }
 }
 
-export {getCurrentWeather, callCurrentData};
+function callCurrentData() {
+    return currentWeatherData;
+};
+
+function callHourlyData() {
+    return hourlyWeatherData;
+};
+
+export { getLatLon, callCurrentData, callHourlyData};
